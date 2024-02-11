ed72f195310c3c59b8e2cb1f89fde91478a52b81
Eryhnar <pedrofernbel@gmail.com>
2024-02-11 20:49:45 +0100

    Added winner check function.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const appState = {
         ['', '', ''],
         ['', '', '']
     ],
-    winner: null,
+    winner: false,
+    winnerName: null,
 
     // change turn
     changeTurn: () => {
@@ -35,11 +36,12 @@ class Player {
 
 // check for winner
 
-const checkWin = (player) => {
+const checkWin = (player) => { //expects player object
     const winCond = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]];
     for (let cond of winCond) {
         if (player.moves.includes(cond[0]) && player.moves.includes(cond[1]) && player.moves.includes(cond[2])) {
-            return player;
+            winner = true;
+            winnerName = player.name;
         } else if (player1.moves.length + player2.moves.length === 9) {
             return 'draw';
         }
